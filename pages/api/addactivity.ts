da7dc0a0d9e1a15b081ae1b850ff6cf1bd15d3c9
metalentco <metalentco777@gmail.com>
2022-06-26 20:00:31 -0800

    cors fixed

--- pages/api/addactivity.ts
+++ pages/api/addactivity.ts
@@ -45,8 +45,15 @@ export default async function handler(
   }
 
   try {
-    const { topic, title, description, url, images, contractAddress, walletAddress } =
-      req.body;
+    const {
+      topic,
+      title,
+      description,
+      url,
+      images,
+      contractAddress,
+      walletAddress,
+    } = req.body;
 
     let userId = GETSTREAM_OWNER_ID;
     if (walletAddress) {
@@ -96,3 +103,25 @@ export default async function handler(
     res.status(400).json({ error: e?.message });
   }
 }
+
+const allowCors = (fn: any) => async (req: any, res: any) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+module.exports = allowCors(handler);
