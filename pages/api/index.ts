06695f5cb980a644cec12c315200264fa927f990
metalentco <metalentco777@gmail.com>
2022-06-26 19:37:20 -0800

    cors fixed

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,8 +1,18 @@
-export default async (req: any, res: any) => {
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  ok: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
   const { method } = req;
 
   // This will allow OPTIONS request
   if (method === "OPTIONS") {
-    return res.status(200).send("ok");
+    return res.status(200).send({ ok: "success" });
   }
-};
+}
